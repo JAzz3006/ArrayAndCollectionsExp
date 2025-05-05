c8cbec390cb522b9fdd823b4599154ab4ffc448a
Grigoriy Chernykh <che90@mail.ru>
2025-05-05 16:52:37 +0500

    reversing array with input mismatch - 05/05 17-00

--- src/ArrayMain.java
+++ src/ArrayMain.java
@@ -1,29 +1,47 @@
 import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class ArrayMain {
-    public static final float DISCOUNT = 0.10f;
+    public static final int NUMBERS_RANGE = 100;
+
     public static void main(String[] args) {
-        Product[] prodArray = new Product[10];
-        Product prod1 = new Product("Laptop", 450f);
-        Product prod2 = new Product("Flash card", 12.5f);
-        Product prod3 = new Product("Keyboard", 39.99f);
-        prodArray[0] = prod1;
-        prodArray[1] = prod2;
-        prodArray[2] = prod3;
-        for (int i = 0; i < prodArray.length; ++i){
-            if (prodArray[i] != null){
-                float priceCheck = prodArray[i].getPrice();
-                if (priceCheck > 200){
-                    prodArray[i].setPrice(priceCheck * (1 - DISCOUNT));
-                }
-                System.out.println(prodArray[i]);
-            };
+        int arraySize = 0;
+        int [] initialArray = new int[3];
+        try{
+            arraySize = getArraySize();
+            initialArray = fillInitialArray(arraySize);
+        } catch (InputMismatchException | NegativeArraySizeException e) {
+            System.out.println("input missmatch !");
+            arraySize = getArraySize();
+            initialArray = fillInitialArray(arraySize);
         }
-        Arrays.fill(prodArray, prod1);
-        for (Product product : prodArray){
-            System.out.println(product);}
-        prod1.setName("Portabe computer");
-        System.out.println(Arrays.toString(prodArray));
+
+        int [] reverseArray = getReverseArray(initialArray);
+        System.out.println(Arrays.toString(initialArray));
+        System.out.println(Arrays.toString(reverseArray));
+
+    }
+
+    public static int getArraySize() throws NegativeArraySizeException, InputMismatchException{
+        System.out.println("Введите размер массива - целое положительное число");
+        return new Scanner(System.in).nextInt();
+    }
+
+    public static int[] fillInitialArray(int arraySize){
+        int[] initialArray = new int[arraySize];
+        for (int i = 0; i < initialArray.length; ++i){
+            initialArray[i] = (int) (Math.random() * 100);
+        }
+        return  initialArray;
+    }
+    public static int[] getReverseArray(int[] initialArray){
+        int[] reverseArray = new int[initialArray.length];
+        int maxIndex = initialArray.length - 1;
+        for (int i = 0; i < initialArray.length; ++i){
+            reverseArray[i] = initialArray[maxIndex - i];
+        }
+        return reverseArray;
     }
 
 }
